c258cfdd24e10df6d54f2537ffa455a68b13bdc4
Aiden Egglin <uefadude@gmail.com>
2025-04-28 22:55:11 -0400

    Finish creating window

--- src/Constants.java
+++ src/Constants.java
@@ -1,5 +1,5 @@
 public class Constants {
-    public static final int SCREEN_WIDTH = 1320;
-    public static final int SCREEN_HEIGHT = 1000;
+    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 600;
     public static final String SCREEN_TITLE = "Snake";
 }
--- src/Window.java
+++ src/Window.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JFrame implements Runnable{
+
+    public boolean isRunning;
 
     public Window(int width, int height, String title) {
         setSize(width, height);
@@ -8,10 +11,35 @@ public class Window extends JFrame implements Runnable{
         setResizable(false);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        isRunning = true;
+    }
+
+    public void update(double dt) {
+        Image dbImage = createImage(getWidth(), getHeight());
+        Graphics dbg = dbImage.getGraphics();
+        this.draw(dbg);
+        getGraphics().drawImage(dbImage, 0, 0, this);
+    }
+
+    public void draw(Graphics g) {
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect(0, 0, getWidth(), getHeight());
     }
 
     @Override
     public void run() {
-
+        double lastFrameTime = 0.0;
+        try {
+            while (isRunning) {
+                double time = Time.getTime();
+                double deltaTime = time - lastFrameTime;
+                lastFrameTime = time;
+                update(deltaTime);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
